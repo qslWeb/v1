c45b466d65ffcfa03a48791a4b7043c5dd0a7040
qslWeb <foodtops@163.com>
2019-01-10 11:02:43 +0800

    cs2
    修正

--- meta.js
+++ meta.js
@@ -169,7 +169,6 @@ module.exports = {
     'test/unit/specs/index.js': "unit && runner === 'karma'",
     'test/unit/setup.js': "unit && runner === 'jest'",
     'test/e2e/**/*': 'e2e',
-    'src/router/**/*': 'router',
     'src/demo/**/*': 'demo',
   },
   complete: function(data, { chalk }) {
